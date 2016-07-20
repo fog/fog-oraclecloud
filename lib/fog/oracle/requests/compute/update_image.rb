e3036065ddc9d290c614bfaddaa887a835059130
Joel Nation <joel.nation@outlook.com>
2016-07-21 08:16:57 +1000

    Added update_image

---
+++ lib/fog/oracle/requests/compute/update_image.rb
@@ -0,0 +1,27 @@
+module Fog
+  module Compute
+    class Oracle
+      class Real
+      	def update_image (name, options={})
+          # Just in case it's already set
+          name.sub! "/Compute-#{@identity_domain}/#{@username}/", ''
+          body_data     = {
+            'name'             		=> "/Compute-#{@identity_domain}/#{@username}/#{name}",
+            'description'					=> options[:description],
+            'default'					   	=> options[:default]
+          }
+          body_data = body_data.reject {|key, value| value.nil?}
+          request(
+            :method   => 'PUT',
+            :expects  => 200,
+            :path     => "/imagelist/Compute-#{@identity_domain}/#{@username}/#{name}",
+            :body     => Fog::JSON.encode(body_data),
+            :headers  => {
+              'Content-Type' => 'application/oracle-compute-v3+json'
+            }
+          )
+      	end
+      end
+    end
+  end
+end
